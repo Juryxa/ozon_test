50cda1494869829bfb8a64e8a1727a2ecaedd5a9
Vyacheslav <101479422+Juryxa@users.noreply.github.com>
2025-04-06 16:03:49 +0300

    script improvement

--- script.js
+++ script.js
@@ -4,32 +4,28 @@ const progressFill = document.getElementById("progressFill");
 const valueInput = document.getElementById("valueInput");
 const animateToggle = document.getElementById("animateToggle");
 const hideToggle = document.getElementById("hideToggle");
+
 const radius = progressFill.r.baseVal.value;
 const circumference = 2 * Math.PI * radius;
-
-
-document.addEventListener("DOMContentLoaded", () => {
-    const block = new ProgressBlock(".progress-circle-container", "#valueInput", ".progress-fill");
-
-    animateToggle.addEventListener("change", (e) => {
-        e.target.checked ? block.startRotation() : block.stopRotation();
-    });
-
-    hideToggle.addEventListener("change", (e) => {
-        e.target.checked ? block.hide() : block.show();
-    });
-
-    valueInput.addEventListener("input", function (){
-        setProgress(block.getValue())
-    })
-});
 
 progressFill.style.strokeDasharray = `${circumference} ${circumference}`;
 progressFill.style.strokeDashoffset = circumference;
 
-function setProgress(value){
-    let offset = circumference - (value / 100) * circumference;
+const block = new ProgressBlock(".progress-circle-container", "#valueInput", ".progress-fill");
+
+animateToggle.addEventListener("change", (e) => {
+    e.target.checked ? block.startRotation() : block.stopRotation();
+});
+
+hideToggle.addEventListener("change", (e) => {
+    e.target.checked ? block.hide() : block.show();
+});
+
+valueInput.addEventListener("input", () => {
+    setProgress(block.getValue());
+});
+
+function setProgress(value) {
+    const offset = circumference - (value / 100) * circumference;
     progressFill.style.strokeDashoffset = offset;
 }
-
-
